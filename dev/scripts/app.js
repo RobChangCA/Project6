651ec8b9addd29684f4b1f1f519459b79b6d0617
Rob Chang <robchangcanada@gmail.com>
2017-09-09 17:07:56 -0400

    fixed functionality, added aphrodite, added more sliders

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StyleSheet, css } from 'aphrodite';
 import { 
     BrowserRouter as Router, 
     Route, Link, NavLink } from 'react-router-dom';
@@ -10,71 +11,98 @@ class App extends React.Component{
 		super();
 		this.state = {
 			initial__rotation: 0,
-			initial__color: "blue",
+			initial__color: "red",
 			initial__scale: 1,
+			initial__shape: 1,
 			final__rotation: 0,
 			final__color: "red",
 			final__scale: 1,
+			final__shape: 1,
+			duration: 1,
 		};
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 
 	handleChange(event){
-		console.log(event.target)
+		console.log(event.target.value)
 		this.setState({
 			[event.target.id] : event.target.value
 		})
 	}
 	render(){
-		const styling = { 
-				width: 100px;
-				height: 100px;
-				animation: anime 5s linear infinite;
-				@keyframes anime{
-					0%{
-						transform: scale(this.state.initial__rotation), rotate(this.state.initial__rotation);
-						color: this.state.initial__color;
-					}
-					100%{
-						transform: scale(this.state.final__rotation), rotate(this.state.final__rotation);
-						color: this.state.final__color;
-					}
-				}		
-		}
+		const styling = StyleSheet.create({  
+			anime: {
+				height: '250px',
+				width: '250px',
+				animationName: {
+						'0%' : {
+							transform: `scale(${this.state.initial__scale}) rotate(${this.state.initial__rotation}deg)`,
+							background: this.state.initial__color,
+							borderRadius: `${this.state.initial__shape}%`,
+						},
+						'100%' : {
+							transform: `scale(${this.state.final__scale}) rotate(${this.state.final__rotation}deg)`,
+							background: this.state.final__color,
+							borderRadius: `${this.state.final__shape}%`,
+						}
+				},
+				animationDuration: `${this.state.duration}s`,
+				animationIterationCount: 'infinite',	
+			}
+		})
+		
 		
 		return(
-			<div>
+			<div className="wrapper">
 				<section className="animationFrame">
-					<div className="animation" style={styling}></div>
+					<div className={css(styling.anime)}></div>
 				</section>
 				<section className="inputControl">
-					<div className="inputControl__initial">
-						<form>
-							<input 
-								type="range" 
-								id="initial__rotation" 
-								min="0" 
-								max="360" 
-								onChange={this.handleChange}
-							/>
-							<input 
-								type="text" 
-								id="initial__color"  
-								onChange={this.handleChange} 
-							/>
-							<input 
-								type="range" 
-								id="initial__scale" 
-								min="0" 
-								max="2"
-								step=".1"
-								onChange={this.handleChange} 
-							/>
-						</form>
-					</div>
-					<div className="inputControl__final">
-						<form>
+					<form className="initial__input">
+						<label htmlFor="duration"> Duration</label>
+						<input 
+							type="range" 
+							id="duration" 
+							min="0" 
+							max="60" 
+							onChange={this.handleChange}
+						/>
+						<label htmlFor="initial__rotation"> Rotation</label>
+						<input 
+							type="range" 
+							id="initial__rotation" 
+							min="0" 
+							max="360" 
+							onChange={this.handleChange}
+						/>
+						<label htmlFor="initial__color">Starting Color</label>
+						<select name="initial__color" id="initial__color" onChange={this.handleChange}>
+							<option value="red">Red</option>
+							<option value="green">green</option>
+							<option value="blue">blue</option>
+						</select>
+						<label htmlFor="initial__scale">Starting Scale</label>
+						<input 
+							type="range" 
+							id="initial__scale" 
+							min="0" 
+							max="2"
+							step=".1"
+							onChange={this.handleChange} 
+						/>
+						<label htmlFor="initial__shape">Starting Shape</label>
+						<input 
+							type="range" 
+							id="initial__shape" 
+							min="0" 
+							max="100"
+							step="1"
+							onChange={this.handleChange} 
+						/>
+					</form>
+						<form className='final__input'>
+							<label htmlFor="final__rotation">Final Rotation</label>
 							<input 
 								type="range" 
 								id="final__rotation" 
@@ -82,11 +110,13 @@ class App extends React.Component{
 								max="360" 
 								onChange={this.handleChange} 
 							/>
-							<input 
-								type="text" 
-								id="final__color" 
-								onChange={this.handleChange} 
-							/>
+							<label htmlFor="final__color">Final Color</label>
+							<select name="final__color" id="final__color" onChange={this.handleChange}>
+								<option value="red">Red</option>
+								<option value="green">green</option>
+								<option value="blue">blue</option>
+							</select>
+							<label htmlFor="final__scale">Final scale</label>
 							<input 
 								type="range" 
 								id="final__scale" 
@@ -95,12 +125,21 @@ class App extends React.Component{
 								step=".1" 
 								onChange={this.handleChange} 
 							/>
+							<label htmlFor="final__shape">Final Shape</label>
+							<input 
+								type="range" 
+								id="final__shape" 
+								min="0" 
+								max="100"
+								step="1"
+								onChange={this.handleChange} 
+							/>
 						</form>
-					</div>
 				</section>
 			</div>
 		)
 	}
+
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
