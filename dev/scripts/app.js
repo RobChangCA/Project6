96cb375fc83d25aef2fcc118864af1e3539d21ba
Rob Chang <robchangcanada@gmail.com>
2017-09-11 17:02:39 -0400

    added firebase delete and onscreen functionality

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -25,16 +25,70 @@ class App extends React.Component{
 			final__color: "#C44D58",
 			final__scale: 1,
 			final__shape: 1,
+			storedKeyframes: [],
 		};
 		this.handleChange = this.handleChange.bind(this);
+		this.removeItem = this.removeItem.bind(this);
+	}
+	componentDidMount(){
+		dbRef.on('value', (snapshot) => {
+			const newKeyframeArray=[];
+			const firebaseItems = snapshot.val();
+			for (let key in firebaseItems) {
+				const firebaseItem = (firebaseItems[key]);
+				firebaseItem.id=key;
+				let newKeyframe = {
+					transition: 'all 1s',
+					animationName: {
+							'0%' : {
+								height: `${firebaseItem.initial__height}px`,
+								width: `${firebaseItem.initial__width}px`,
+								transform: `scale(${firebaseItem.initial__scale}) rotate(${firebaseItem.initial__rotation}deg)`,
+								background: `${firebaseItem.initial__color}`,
+								borderRadius: `${firebaseItem.initial__shape}%`,
+							},
+							'50%' : {
+								height: `${firebaseItem.final__height}px`,
+								width: `${firebaseItem.final__width}px`,
+								transform: `scale(${firebaseItem.final__scale}) rotate(${firebaseItem.final__rotation}deg)`,
+								background: `${firebaseItem.final__color}`,
+								borderRadius: `${firebaseItem.final__shape}%`,
+							},
+							'100%' : {
+								height: `${firebaseItem.initial__height}px`,
+								width: `${firebaseItem.initial__width}px`,
+								transform: `scale(${firebaseItem.initial__scale}) rotate(${firebaseItem.initial__rotation}deg)`,
+								background: `${firebaseItem.initial__color}`,
+								borderRadius: `${firebaseItem.initial__shape}%`,
+							},
+					},
+					animationDuration: `${firebaseItem.duration}s`,
+					animationIterationCount: 'infinite',
+					duration: `${firebaseItem.duration}`,
+					initial__width: `${firebaseItem.initial__width}`,
+					initial__height: `${firebaseItem.initial__height}`,
+					initial__rotation: `${firebaseItem.initial__rotation}`,
+					initial__color: `${firebaseItem.initial__color}`,
+					initial__scale: `${firebaseItem.initial__scale}`,
+					initial__shape: `${firebaseItem.initial__shape}`,
+					final__width: `${firebaseItem.final__width}`,
+					final__height: `${firebaseItem.final__height}`,
+					final__rotation: `${firebaseItem.final__rotation}`,
+					final__color: `${firebaseItem.final__color}`,
+					final__scale: `${firebaseItem.final__scale}`,
+					final__shape: `${firebaseItem.final__shape}`,
+				}
+				newKeyframeArray.push(newKeyframe);
+			}
+			this.setState({
+				storedKeyframes: newKeyframeArray,
+			})
+		})
 	}
 	handleChange(event){
 		this.setState({
 			[event.target.id] : event.target.value
 		})
-	}
-	cssOutputString(){
-
 	}
 	fbUpload(e) {
 			e.preventDefault();
@@ -44,6 +98,32 @@ class App extends React.Component{
 
 			dbRef.push(this.state);
 		}
+	removeItem(key) {
+		console.log(key._definition.id);
+		const itemRef = firebase.database().ref(`/animations/${key._definition.id}`);
+		itemRef.remove();
+	}
+	onScreen(key){
+		const e = key._definition;
+		console.log(key);
+		this.setState({
+			duration: e.duration,
+			initial__width: e.initial__width,
+			initial__height: e.initial__height,
+			initial__rotation: e.initial__rotation,
+			initial__color: e.initial__color,
+			initial__scale: e.initial__scale,
+			initial__shape: e.initial__shape,
+			final__width: e.final__width,
+			final__height: e.final__height,
+			final__rotation: e.final__rotation,
+			final__color: e.final__color,
+			final__scale: e.final__scale,
+			final__shape: e.final__shape,
+		})
+		
+		
+	}
 	render(){
 		const styling = StyleSheet.create({  
 			anime: {
@@ -78,6 +158,7 @@ class App extends React.Component{
 
 		return(
 			<div>
+				<h1 className='title'>Animation</h1>
 				<div className="wrapper">
 					<section className="animationFrame">
 						<div className={css(styling.anime)}></div>
@@ -89,7 +170,8 @@ class App extends React.Component{
 								type="range" 
 								id="duration" 
 								min="1" 
-								max="20" 
+								max="20"
+								value={this.state.duration} 
 								onChange={this.handleChange}
 							/>
 							<label htmlFor="initial__height">Starting Height</label>
@@ -98,6 +180,7 @@ class App extends React.Component{
 								id='initial__height'
 								min='1'
 								max='300'
+								value={this.state.initial__height}
 								onChange={this.handleChange}
 							/>
 							<label htmlFor="initial__width">Starting Width</label>
@@ -106,6 +189,7 @@ class App extends React.Component{
 								id='initial__width'
 								min='1'
 								max='300'
+								value={this.state.initial__width}
 								onChange={this.handleChange}
 							/>
 							<label htmlFor="initial__rotation"> Rotation</label>
@@ -115,6 +199,8 @@ class App extends React.Component{
 								min="0" 
 								max="360" 
 								onChange={this.handleChange}
+								value={this.state.initial__rotation}
+								value={this.state.initial__rotation}
 							/>
 							<label htmlFor="initial__color">Starting Color</label>
 							<select name="initial__color" id="initial__color" onChange={this.handleChange}>
@@ -129,6 +215,7 @@ class App extends React.Component{
 								min="0" 
 								max="2"
 								step=".1"
+								value={this.state.initial__scale}
 								onChange={this.handleChange} 
 							/>
 							<label htmlFor="initial__shape">Starting Shape</label>
@@ -138,6 +225,7 @@ class App extends React.Component{
 								min="0" 
 								max="100"
 								step="1"
+								value={this.state.initial__shape}
 								onChange={this.handleChange} 
 							/>
 						</form>
@@ -148,6 +236,7 @@ class App extends React.Component{
 								id='final__height'
 								min='1'
 								max='300'
+								value={this.state.final__height}
 								onChange={this.handleChange}
 							/>
 							<label htmlFor="final__width">Final Width</label>
@@ -156,6 +245,7 @@ class App extends React.Component{
 								id='final__width'
 								min='1'
 								max='300'
+								value={this.state.final__width}
 								onChange={this.handleChange}
 							/>
 							<label htmlFor="final__rotation">Final Rotation</label>
@@ -164,6 +254,7 @@ class App extends React.Component{
 								id="final__rotation" 
 								min="0" 
 								max="360" 
+								value={this.state.final__rotation}
 								onChange={this.handleChange} 
 							/>
 							<label htmlFor="final__color">Final Color</label>
@@ -179,6 +270,7 @@ class App extends React.Component{
 								min="0" 
 								max="2"
 								step=".1" 
+								value={this.state.final__scale}
 								onChange={this.handleChange} 
 							/>
 							<label htmlFor="final__shape">Final Shape</label>
@@ -188,36 +280,51 @@ class App extends React.Component{
 								min="0" 
 								max="100"
 								step="1"
+								value={this.state.final__shape}
 								onChange={this.handleChange} 
 							/>
 						</form>
-						<button onClick={this.fbUpload.bind(this)}>Click me</button>
+						<button onClick={this.fbUpload.bind(this)}>Save Animation</button>
 					</section>
 				</div>
 				<section className='cssOutput'>
 					<p className='cssOutput__field'>
-							{`@keyframes AnimationName '0%' : {
-							height: ${this.state.initial__height}px,
-							width: ${this.state.initial__width}px,
-							transform: scale(${this.state.initial__scale}) rotate(${this.state.initial__rotation}deg),
-							background: ${this.state.initial__color},
-							borderRadius: ${this.state.initial__shape}%,
+							{`@keyframes AnimationName{ 0%{
+							height: ${this.state.initial__height}px;
+							width: ${this.state.initial__width}px;
+							transform: scale(${this.state.initial__scale}) rotate(${this.state.initial__rotation}deg);
+							background: ${this.state.initial__color};
+							border-radius: ${this.state.initial__shape}%;
+						}
+						50%{
+							height: ${this.state.final__height}px;
+							width: ${this.state.final__width}px;
+							transform: scale(${this.state.final__scale}) rotate(${this.state.final__rotation}deg);
+							background: ${this.state.final__color};
+							border-radius: ${this.state.final__shape}%;
 						},
-						'50%' : {
-							height: ${this.state.final__height}px,
-							width: ${this.state.final__width}px,
-							transform: scale(${this.state.final__scale}) rotate(${this.state.final__rotation}deg),
-							background: ${this.state.final__color},
-							borderRadius: ${this.state.final__shape}%,
-						},
-						'100%' : {
-							height: ${this.state.initial__height}px,
-							width: ${this.state.initial__width}px,
-							transform: scale(${this.state.initial__scale}) rotate(${this.state.initial__rotation}deg),
-							background: ${this.state.initial__color},
-							borderRadius: ${this.state.initial__shape}%,
-						},`}
+						100%{
+							height: ${this.state.initial__height}px;
+							width: ${this.state.initial__width}px;
+							transform: scale(${this.state.initial__scale}) rotate(${this.state.initial__rotation}deg);
+							background: ${this.state.initial__color};
+							border-radius: ${this.state.initial__shape}%;
+						}}`}
 					</p>
+				</section>
+				<section className="animationGallery">
+						{this.state.storedKeyframes.map(keyframe=>{
+							const style =  StyleSheet.create({ 
+								keyframe
+							})
+							return(
+								<div className="animationGallery__container">
+									<div className={css(style.keyframe)}></div>
+									<button className='animationGallery__delete' onClick={() => this.removeItem(style.keyframe)}>Delete</button>
+									<button className="animationGallery__onScreen" onClick={()=> this.onScreen(style.keyframe)}>On screen</button>
+								</div>
+							)
+						})}
 				</section>
 			</div>
 		)
@@ -226,3 +333,5 @@ class App extends React.Component{
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// <button className='animationGallery__delete' onClick={() => this.removeItem(this.state.storedKeyframes), console.log(this.state.storedKeyframes)}>Delete</button>
